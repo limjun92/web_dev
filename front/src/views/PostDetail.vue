8fbc8fbd230e6048c41847e263b6093b98b8f493
limjun92 <limjun9205@naver.com>
2023-02-07 17:26:08 +0900

    20230207_bold

--- front/src/views/PostDetail.vue
+++ front/src/views/PostDetail.vue
@@ -1,13 +1,44 @@
 <template>
-    <h1>PostDetail</h1>
+    <div>
+        <h1>PostDetail</h1>
+        <div class="editor-menu">
+            <button onclick="document.execCommand('bold')">
+                <b>B</b>
+            </button>
+            <button onclick="document.execCommand('Italic')">
+                <i>I</i>
+            </button>
+            <button onclick="document.execCommand('Underline')">
+                <u>U</u>
+            </button>
+            <button onclick="document.execCommand('StrikeThrough')">
+                <s>S</s>
+            </button>
+        </div>
+
+        <div class = "content" contenteditable="true">
+            <p>
+                <br>
+            </p>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-
+    methods:{
+        bold(){
+            console.log(window.getSelection());
+            document.execCommand('bold')
+        }
+    }
 }
 </script>
 
 <style>
-
+.content{
+    border: 1px solid;
+    width:100%;
+    height: 400px;
+}
 </style>
